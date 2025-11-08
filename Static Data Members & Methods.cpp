d01b958bf0f65f9d51de6dccae72b6acc0d121f0
r4coder <ritheeshk2003@gmail.com>
2025-11-08 15:37:55 +0530

    Implement static data members and methods in class

--- Static Data Members & Methods.cpp
+++ Static Data Members & Methods.cpp
@@ -1,1 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class x
+{
+    int id;
+    static int count;
+    
+    
+    
+    public :
+    void setData(void);
+    void getData(void);
+    
+    
+    
+    
+
+  
+};
+
+int x::count;
+
+void x::setData(void)
+{
+   cout<<"Enter the employee id:";
+   cin>>id;
+   count++;
+}
+
+void x::getData(void)
+    {
+   cout<<"emp id,emp no:";
+   cout<<id<<",";
+   cout<<count;
+        
+        
+    }
+
+
+
+int main()
+{
+    x em1,em2,em3;
+    em1.setData();
+    em2.setData();
+    em2.getData();
+    em3.setData();
+   
+  
+   
+   
+    
+    
+    
+return 0;    
+}
+    
+    
+    
+    
+    
 

e9c122fd9e2eb7e1eabb726b5edf09f25dd18b0e
r4coder <ritheeshk2003@gmail.com>
2025-11-08 15:58:27 +0530

    Add methods to set and get data in class x
    Implement class methods to set and get data from objects.

--- 5.Array of Objects & Passing Objects as Function Arguments.cpp
+++ 5.Array of Objects & Passing Objects as Function Arguments.cpp
@@ -1,1 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class x
+{
+    int a,b;
+   
+   public:
+   
+   
+void setData(int y,int z)
+{
+   a=y;
+   b=z;
+}
+
+void getData(x c1,x c2)
+    {
+       a=c1.a+c2.a;
+       b=c1.b+c2.b;
+       cout<<"re,im:"<<a<<" "<<b;
+    }
+    
+    
+};
+
+
+
+
+
+
+
+
+int main()
+{
+    x c1,c2,c3;
+    c1.setData(1,2);
+    c2.setData(3,4);
+    c3.getData(c1,c2);
+    
+   
+  
+   
+   
+    
+    
+    
+return 0;    
+}
+    
+    
+    
+    
+    
 

cda058c73d68fd97c9cb49867973081368593237
r4coder <ritheeshk2003@gmail.com>
2025-11-08 15:17:14 +0530

    Implement item price management in class x

--- Objects Memory Allocation & using Arrays in Classes.cpp
+++ Objects Memory Allocation & using Arrays in Classes.cpp
@@ -1,1 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class x
+{
+    int itemid[100];
+    int itemprice[100];
+    int counter;
+    
+    
+    public :
+    void initcounter(void){counter=0;}
+    void setPrice(void);
+    void displayPrice(void);
+    
+    
+    
+
+  
+};
+
+void x::setPrice(void)
+{
+   cout<<"Enter the itemid:";
+   cin>>itemid[counter];
+   cout<<"Enter the item price:";
+   cin>>itemprice[counter];
+   counter++;
+}
+
+void x::displayPrice(void)
+    {
+             
+        for(int i=0;i<3;i++)
+        {
+            cout<<"The Price of Item with Item Id:"<<itemid[i]<<","<<itemprice[i];
+            
+        }
+        
+        
+    }
+
+
+
+int main()
+{
+    x shop;
+    shop.initcounter();
+   for(int i=0;i<3;i++)
+   {
+       shop.setPrice();
+   }
+   shop.displayPrice();
+   
+    
+    
+    
+return 0;    
+}
+    
+    
+    
+    
+    
 

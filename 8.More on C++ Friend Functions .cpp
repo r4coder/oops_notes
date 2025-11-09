223fd05534e72a52ebcf26a9fe10598de1e2085f
r4coder <ritheeshk2003@gmail.com>
2025-11-09 22:39:14 +0530

    Add basic C++ class with print function

--- 8.More on C++ Friend Functions .cpp
+++ 8.More on C++ Friend Functions .cpp
@@ -1,1 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+class x
+{
+    int a,b;
+    
+    public :
+    x(void);
+    
+    void printdata(void)
+    {
+        cout<<a<<","<<b<<endl;
+    }
+    
+    
+    
+};
+
+x::x(void){
+    a=10;
+    b=0;
+}
+
+int main()
+{
+    x t;
+    t.printdata();
+    
+    
+    
+    
+return 0;    
+}
+    
+    
+    
+    
+    

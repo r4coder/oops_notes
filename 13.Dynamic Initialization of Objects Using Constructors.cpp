6d60d9dacf57b9adc7d9044178a2c54517f75dc6
r4coder <ritheeshk2003@gmail.com>
2025-11-11 13:12:47 +0530

    Implement dynamic initialization using constructors
    Added a class 'x' with a constructor and a method to display values.

--- 13.Dynamic Initialization of Objects Using Constructors.cpp
+++ 13.Dynamic Initialization of Objects Using Constructors.cpp
@@ -1,1 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+
+class x
+{
+    int a,b,z=1;
+     
+     public:
+       x(int x,int y)
+       {
+          a=x;
+          b=y;
+          
+          for(int i=0;i<4;i++)
+          {
+              z=a*z;
+          }
+          
+          
+       }
+       
+       void getnum(void)
+       {
+           cout<<"a,b,z:"<<" "<<a<<" "<<b<<" "<<z;
+       }
+       
+       
+
+        
+    
+    
+    
+    
+    
+    
+};
+
+
+
+
+
+
+
+
+
+int main()
+{
+    x t(2,1);
+    t.getnum();
+    
+   
+    
+    
+    
+    
+    
+return 0;    
+}

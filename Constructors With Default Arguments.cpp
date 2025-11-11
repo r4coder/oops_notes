2085c4358136f913eeea5f6615cf1d86f74f9972
r4coder <ritheeshk2003@gmail.com>
2025-11-11 12:57:14 +0530

    Implement class with constructor and default arguments
    Added a class with a constructor that supports default arguments.

--- Constructors With Default Arguments.cpp
+++ Constructors With Default Arguments.cpp
@@ -1,1 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+
+class x
+{
+    int a,b,c,d;
+     
+     public:
+       x(int f,int g,int h=9,int i=8)
+       {
+          a=f;
+          b=g;
+          c=h;
+          d=i;
+          
+       }
+       
+       void getnum(void)
+       {
+           cout<<"a,b,c,d"<<a<<b<<c<<d;
+       }
+
+        
+    
+    
+    
+    
+    
+    
+};
+
+
+
+
+
+
+
+
+
+int main()
+{
+    x t(1,2,3,4);
+    t.getnum();
+    
+   
+    
+    
+    
+    
+    
+return 0;    
+}

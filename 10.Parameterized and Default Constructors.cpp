0fbfc33d443ce59484d73ed40bd430dacf2cd414
r4coder <ritheeshk2003@gmail.com>
2025-11-11 12:36:09 +0530

    Add class with parameterized constructor and display method
    Implement a class with parameterized constructor and method to display values.

---
+++ 10.Parameterized and Default Constructors.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class x
+{
+    int a,b;
+     
+     public:
+       x(int x,int y)
+       {
+           a=x;
+           b=y;
+       }
+       
+       void getnum(void)
+       {
+           cout<<"a is"<<a<<"b is"<<b;
+       }
+
+        
+    
+    
+    
+    
+    
+    
+};
+
+
+
+
+
+
+
+
+
+int main()
+{
+    x t(1,2);
+    t.getnum();
+    
+    x u=x(3,4);
+    u.getnum();
+    
+    
+    
+    
+    
+return 0;    
+}

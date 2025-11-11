133be700b9a1678d87a335851c95dd5014169830
r4coder <ritheeshk2003@gmail.com>
2025-11-11 12:45:46 +0530

    Add constructor overloading to class 'x'
    Implemented constructor overloading for class 'x' with three constructors.

--- 11.Constructor Overloading .cpp
+++ 11.Constructor Overloading .cpp
@@ -1,1 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+
+class x
+{
+    int a,b;
+     
+     public:
+       x(int x,int y)
+       {
+           a=x;
+           b=y;
+       }
+       
+       x(int x)
+       {
+           a=x;
+           b=0;
+       }
+       
+       x()
+       {
+           a=0;
+           b=0;
+       }
+       
+       void getnum(void)
+       {
+           cout<<"a is"<<a<<"b is"<<b;
+       }
+
+        
+    
+    
+    
+    
+    
+    
+};
+
+
+
+
+
+
+
+
+
+int main()
+{
+    x t(1,2);
+    x u(1);
+    x v;
+    u.getnum();
+    
+   
+    
+    
+    
+    
+    
+return 0;    
+}
